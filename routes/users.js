00b095c9b69f68b56b7ab06077304da5ddefdfcf
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-07-13 17:46:53 +0200

    implemented db

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next()
 });
+
+router.get('/gut', (req, res, next) => {
+  res.send('u gut')
+})
 
 module.exports = router;
