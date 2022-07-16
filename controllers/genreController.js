4cf622b0565d46ace05e98d7ac435b11b07f4c95
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-07-16 14:48:21 +0200

    added genre delete

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -95,13 +95,47 @@ exports.genre_create_post =  [
 ];  
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+exports.genre_delete_get = function(req, res, next) {
+    
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id).exec(callback)
+        },
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err) }
+        if (results.genre==null) {
+            res.redirect('/calalog/genres')
+        }
+        res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books })
+    })
 };
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+    
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.body.genreid).exec(callback)
+        },
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.body.genreid }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err) }
+        if (results.genre_books.length > 0) {
+            res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books })
+            return;
+        }
+        else {
+            Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+                if (err) { return next(err) }
+                res.redirect('/catalog/genres')
+            })
+        }
+    })
 };
 
 // Display Genre update form on GET.
