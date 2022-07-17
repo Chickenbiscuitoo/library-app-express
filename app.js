e34f65babbcbdd3a162ecd2181b2a719cdf76d4d
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-07-17 13:13:50 +0200

    fixed create book issue

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose')
-const dev_db_url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@libraryapp.8p5t6.mongodb.net/?retryWrites=true&w=majority`
-const mongoDB = process.env.MONGODB_URI || dev_db_url
+const mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology:true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
