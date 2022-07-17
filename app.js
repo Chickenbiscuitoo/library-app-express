9aca82a94ccc40450ca59b108b89a240cf313503
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-07-17 12:28:04 +0200

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose')
-const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@libraryapp.8p5t6.mongodb.net/?retryWrites=true&w=majority`
+const dev_db_url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@libraryapp.8p5t6.mongodb.net/?retryWrites=true&w=majority`
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology:true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
